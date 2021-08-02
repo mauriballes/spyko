1f1eb890809cae96101dccfd3ce42a5deda6d663
Mauricio Ballesteros <mballes95@gmail.com>
2021-08-02 05:38:07 -0400

    Fix Tests Detail

--- src/pages/detail/Detail.test.tsx
+++ src/pages/detail/Detail.test.tsx
@@ -7,7 +7,7 @@ import Detail from "./Detail";
 import product from "../../mocks/product.json";
 import routes from "../../router/paths";
 
-test("renders home title", () => {
+test("renders detail title", () => {
   render(
     <MemoryRouter initialEntries={["/detail/1"]}>
       <Route path={routes.detail}>
